96fe27264bffa1f0a82b34a69fad66d987e0f8fc
Chen Qizhi <qchenqizhi@gmail.com>
2013-07-06 07:32:57 +0800

    EmotionAttributedLabel

---
+++ Example/EmotionAttributedLabel.h
@@ -0,0 +1,13 @@
+//
+//  EmotionAttributedLabel.h
+//  Espressos
+//
+//  Created by Chen Qizhi on 7/6/13.
+//  Copyright (c) 2013 Gowalla. All rights reserved.
+//
+
+#import "TTTAttributedLabel.h"
+
+@interface EmotionAttributedLabel : TTTAttributedLabel
+
+@end
---
+++ Example/EmotionAttributedLabel.m
@@ -0,0 +1,31 @@
+//
+//  EmotionAttributedLabel.m
+//  Espressos
+//
+//  Created by Chen Qizhi on 7/6/13.
+//  Copyright (c) 2013 Gowalla. All rights reserved.
+//
+
+#import "EmotionAttributedLabel.h"
+
+@implementation EmotionAttributedLabel
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
